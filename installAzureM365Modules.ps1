88403525a7de2a39cf6363d7f9bebf1102b37727
br3cc <bcantalice93@gmail.com>
2023-12-13 12:38:16 -0500

    Add files via upload

---
+++ installAzureM365Modules.ps1
@@ -0,0 +1,7 @@
+Install-Module -Name Az
+Install-Module -Name ExchangeOnlineManagement
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+Install-Module -Name Microsoft.Graph
+Install-Module -Name Microsoft.Graph.Beta
+Install-Module -Name MicrosoftTeams
+Install-Module -Name PoShLog
